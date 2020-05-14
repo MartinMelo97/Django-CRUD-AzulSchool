5b48798612862e54c68ee7171f97e1e05f7c7dd2
Martin Melo Godinez <martin.melo.dev.97@gmail.com>
2020-05-14 01:25:17 -0500

    CRUD with user and profile onetone

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from .models import UserModel
+from .models import UserModel, Profile
 
 class UserForm(forms.ModelForm):
     class Meta:
         model = UserModel
         fields = '__all__'
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        # fields = ['profile_image', 'bio', 'is_public']
+        exclude = ['user']
--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django import views
 from .models import UserModel
-from .forms import UserForm
+from .forms import UserForm, ProfileForm
 from django.contrib import messages
 
 class GetUsersView (views.View):
@@ -25,25 +25,32 @@ class CreateUserView(views.View):
     template_name = 'main/form.html'
     action = 'create'
     def get(self, request):
-        form = UserForm()
+        user_form = UserForm()
+        profile_form = ProfileForm()
         context = {
-            'form': form,
+            'user_form': user_form,
+            'profile_form': profile_form,
             'action': self.action
         }
         return render(request, self.template_name, context)
 
     def post(self, request):
-        new_form = UserForm(request.POST)
-        if new_form.is_valid():
-            form_data = new_form.save(commit=False)
-            form_data.save()
+        new_user_form = UserForm(request.POST)
+        new_profile_form = ProfileForm(request.POST, request.FILES)
+        if new_user_form.is_valid() and new_profile_form.is_valid():
+            user_form_data = new_user_form.save()
+            profile_form_data = new_profile_form.save(commit=False)
+            profile_form_data.user = user_form_data
+            profile_form_data.save()
             messages.success(request, 'Usuario creado exitósamente!')
             return redirect('user:list')
         else:
-            errors = new_form.errors.as_data()
-            form = UserForm()
+            errors = new_user_form.errors.as_data()
+            user_form = UserForm()
+            profile_form = ProfileForm()
             context = {
-                'form': form,
+                'user_form': user_form,
+                'profile_form': profile_form,
                 'action': self.action
             }
             messages.error(request, 'Algo falló al momento de crear un usuario')
@@ -54,29 +61,35 @@ class UpdateUserView(views.View):
     action = 'update'
     def get(self, request, id):
         user = UserModel.objects.get(id=id)
-        form = UserForm(instance=user)
+        user_form = UserForm(instance=user)
+        profile_form = ProfileForm(instance=user.profile)
         context = {
             'user': user,
             'action': self.action,
-            'form': form
+            'user_form': user_form,
+            'profile_form': profile_form
         }
         return render(request, self.template_name, context)
 
     def post(self, request, id):
         user = UserModel.objects.get(id=id)
-        edit_form = UserForm(request.POST, instance=user)
-        if edit_form.is_valid():
-            form_data = edit_form.save(commit=False)
-            form_data.save()
+        edit_user_form = UserForm(request.POST, instance=user)
+        edit_profile_form = ProfileForm(request.POST, request.FILES, instance=user.profile)
+        if edit_user_form.is_valid() and edit_profile_form.is_valid():
+            edit_user_data = edit_user_form.save()
+            edit_profile_data = edit_profile_form.save()
             messages.success(request, 'Usuario actualizado exitósamente!')
             return redirect('user:detail', id)
         else:
-            errors = edit_form.errors.as_data()
-            form = UserForm(instance=user)
+            errors = edit_user_form.errors.as_data()
+            user = UserModel.objects.get(id=id)
+            user_form = UserForm(instance=user)
+            profile_form = ProfileForm(instance=user.profile)
             context = {
-                'form': form,
+                'user': user,
                 'action': self.action,
-                'user': user
+                'user_form': user_form,
+                'profile_form': profile_form
             }
             messages.error(request, 'Algo falló al editar la información del usuario')
             return render(request, self.template_name, context)
